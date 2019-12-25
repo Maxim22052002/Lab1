bc694cd8f58e9cad35216ce7a68b422664b7a1d3
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 18:27:09 +0300

    T5

---
+++ Number7/Number7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Number7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the number: ");
+                string text = Console.ReadLine();
+
+                char[] letters = text.ToCharArray();
+                Array.Reverse(letters);
+
+                text = String.Concat(letters);
+                int reversed = Convert.ToInt32(text);
+                if (reversed >= 100 && reversed < 1000)
+                {
+                    Console.WriteLine("Reversed: " + reversed);
+                }
+                else { Console.WriteLine("Error! Try again."); }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+            }
+            
+
+
+
+        }
+    }
+}
