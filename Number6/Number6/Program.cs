49971fa2cbb3e3f23cf50ee4e98cac3ab871436d
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 18:26:23 +0300

    T5

---
+++ Number6/Number6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Number6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a four digit number: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                int a1, a2, a3, a4;
+                a1 = x / 1000;
+                a4 = x % 10;
+                a2 = x / 100 % 10;
+                a3 = x / 10 % 10;
+                int result = a1 * a2 * a3 * a4;
+
+                if (x > 999 && x < 10000)
+                {
+                    Console.WriteLine("Result: "+ a1 + '*'+ a2+  '*'+ a3 +'*'+ a4 +'=' + result);    //Console.WriteLine("Result: "+ result);
+                }
+                else
+                {
+                    Console.WriteLine("Oops, error! Try again.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+            }
+             
+
+            
+
+
+
+        }
+    }
+}
