686a3d5052a704d3ad764bc6548737ba9bba8aea
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 18:28:23 +0300

    T7

---
+++ Number8/Number8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Number8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter number x: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                double result = 3 * Math.Pow(x, 4) - 5 * Math.Pow(x, 3) + 2 * Math.Pow(x, 2) - x + 7;
+                Console.WriteLine("Result: " + result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+            }
+        }
+    }
+}
