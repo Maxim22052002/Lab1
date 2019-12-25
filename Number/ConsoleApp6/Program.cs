2c8911a02f75310540a45be1ab728d9d6e3a464d
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 18:34:54 +0300

    T10

---
+++ Number/ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace number1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter fractional number: ");
+                string a;
+                a = Console.ReadLine();
+                double x = Convert.ToDouble(a);
+
+                int d = (int)((x - (int)x) * 10);
+                Console.WriteLine("Result: "+ d);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+                
+            }
+
+
+
+
+        }
+    }
+}
