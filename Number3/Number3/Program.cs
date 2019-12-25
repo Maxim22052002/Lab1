f395198f1196ea44765e5986b00966c627b4ee64
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 18:23:23 +0300

    T3

---
+++ Number3/Number3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Number3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            { try
+                {
+                    Console.WriteLine("Введите число часов, минут и секунд: ");
+                    int hours = Convert.ToInt32(Console.ReadLine());
+                    int min = Convert.ToInt32(Console.ReadLine());
+                    int sec = Convert.ToInt32(Console.ReadLine());
+                    if (hours >= 0 && hours < 12 && min >= 0 && min < 60 && sec >= 0 && sec < 60)
+                    {
+                        double totalH = (double)hours + (double)min / 60 + (double)sec / 3600;
+
+                        Console.WriteLine("Pre reserved: " + totalH);
+                        double corner = totalH * (360 / 12);
+                        Console.WriteLine("Result: " + corner);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("FormatException");
+                }
+            }
+
+        }
+    }
+}
