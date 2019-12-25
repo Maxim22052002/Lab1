cd4d86bf30102da56116928afd30972a7ee3215d
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 18:24:37 +0300

    T3

---
+++ Number4/Number4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Number4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the first number: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the second number: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                Console.WriteLine("Result: " + a + " , " + b);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+            }
+            
+
+        }
+    }
+}
