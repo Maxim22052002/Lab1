c5513e045eb7d898d01e8800fcaf2bd3ebeee2a1
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 18:21:06 +0300

    T2

---
+++ Number2/Number2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Number2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+                
+                Console.WriteLine("Enter the number of seconds: ");
+                int hours, min;
+                int sec = Convert.ToInt32(Console.ReadLine());
+                hours = sec / 3600;
+                min = (sec % 3600) / 60;
+
+                Console.WriteLine("It's " + hours + " hours " + min + " minutes.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+
+            }
+
+                
+            
+
+        }
+    }
+}
